ec682a619e476e37f08f628c6a515add178fc882
taka <taka@gmail.com>
2026-07-15 12:16:04 +0900

    release

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,33 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
+  const [scene, setScene] = useState("station");
+
   return (
     <main>
       <h1>北千住葉大鉄道</h1>
 
-      <p>🚉 北千住葉大駅</p>
+      {scene === "station" && (
+        <>
+          <p>🚉 北千住葉大駅</p>
 
-      <button>
-        ドアを開ける
-      </button>
+          <button
+            onClick={() => setScene("running")}
+          >
+            発車する
+          </button>
+        </>
+      )}
+
+      {scene === "running" && (
+        <>
+          <p>🚃 電車が走っています</p>
+
+          <p>🌳🏠🌳🏠🌳</p>
+        </>
+      )}
     </main>
   );
 }
