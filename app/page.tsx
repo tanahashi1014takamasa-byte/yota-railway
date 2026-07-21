3e4a4483de3fa3f5efa8fecac82d8ee27430dd6b
taka <taka@gmail.com>
2026-07-21 10:32:08 +0900

    release

--- app/page.tsx
+++ app/page.tsx
@@ -464,7 +464,7 @@ steam.play();
   style={{
     position: "absolute",
     bottom: "35px",
-    left: "40%",
+    left: "43%",
     transform: "translateX(-50%)",
     display: "flex",
     gap: "20px",
