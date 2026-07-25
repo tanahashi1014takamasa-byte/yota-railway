9d3e6c34653bc098fc216307df7254f656570f2e
taka <taka@gmail.com>
2026-07-25 12:00:37 +0900

    release

--- app/page.tsx
+++ app/page.tsx
@@ -1333,14 +1333,16 @@ steam.play();
   ))}
 </div>
 
-   <div
+ <div
   style={{
     position: "absolute",
     bottom: "20px",
     left: "50%",
     transform: "translateX(-50%)",
     display: "flex",
-    gap: "30px",
+    justifyContent: "center",
+    gap: "20px",
+    width: "300px",
   }}
 >
 
